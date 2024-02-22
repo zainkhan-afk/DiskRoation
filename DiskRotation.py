fecf72b5d1f6d2bb3d8d6f7b22038be46816a154
Zain Khan <zain.9496@gmail.com>
2024-02-23 02:26:17 +0500

    Optimized code

--- DiskRotation.py
+++ DiskRotation.py
@@ -1,25 +1,107 @@
 import cv2
+import soundfile as sf
 import numpy as np
-
-
+import time
 class DiskRotation:
 	def __init__(self, width, height, disk_radius = 400, rpm = 60, fps = 30):
 		self.Reset(width, height, disk_radius, rpm, fps)
+		
 		self.logo_watermark_path = "grid_landscape_qkmazx.png"
 		self.created_at_watermark_path = "grid_landscape_axkq4b.png"
 		self.logo_text_watermark_path = "grid_landscape_in6hqq.png"
+
+		self.MakeBGWatermarkStensil()
+		self.MakeDiskWatermarkStensil()
 
 		self.logo_watermark_image = cv2.imread(self.logo_watermark_path, cv2.IMREAD_UNCHANGED)
 		self.created_at_watermark_image = cv2.imread(self.created_at_watermark_path, cv2.IMREAD_UNCHANGED)
 		self.logo_text_watermark_image = cv2.imread(self.logo_text_watermark_path, cv2.IMREAD_UNCHANGED)
 
+
 		R, C, _ = self.logo_watermark_image.shape
 		self.logo_watermark_image = cv2.resize(self.logo_watermark_image, (int(1.6 * C), int(1.6 * R)))
 
-		# R, C, _ = self.created_at_watermark_image.shape
-		# self.created_at_watermark_image = cv2.resize(self.created_at_watermark_image, (int(0.75 * C), int(0.75 * R)))
+		# self.created_at_watermark_image = cv2.resize(self.created_at_watermark_image, (int(1.6 * C), int(1.6 * R)))
 
 		self.created_at_watermark_image = self.created_at_watermark_image[:-10, :-10]
+
+
+	def MakeBGWatermarkStensil(self):
+		self.BG_watermark_stensil = np.zeros((self.height, self.width, 3)).astype("uint8")
+		self.BG_watermark_stensil_alpha = np.zeros((self.height, self.width)).astype("uint8")
+
+
+		im1 = cv2.imread(self.logo_watermark_path, cv2.IMREAD_UNCHANGED)
+		R, C, _ = im1.shape
+		im1 = cv2.resize(im1, (int(1.6 * C), int(1.6 * R)))
+		
+		x1 = 25
+		x2 = x1 + im1.shape[1]
+
+		y1 = 25
+		y2 = y1 + im1.shape[0]
+
+		self.BG_watermark_stensil_alpha[y1:y2, x1:x2] = im1[:, :, -1]
+		self.BG_watermark_stensil[y1:y2, x1:x2] = im1[:, :, :-1]
+
+
+
+
+		im2 = cv2.imread(self.created_at_watermark_path, cv2.IMREAD_UNCHANGED)
+		im2 = im2[:-10, :-10,]
+
+
+
+		x2 = self.width
+		x1 = x2 - im2.shape[1]
+
+		y2 = self.height
+		y1 = y2 - im2.shape[0]
+
+		self.BG_watermark_stensil_alpha[y1:y2, x1:x2] = im2[:, :, -1]
+		self.BG_watermark_stensil[y1:y2, x1:x2] = im2[:, :, :-1]
+
+
+		self.BG_watermark_stensil = cv2.bitwise_and(self.BG_watermark_stensil, self.BG_watermark_stensil, mask = self.BG_watermark_stensil_alpha)
+		self.BG_watermark_stensil_alpha_inv = cv2.bitwise_not(self.BG_watermark_stensil_alpha)
+
+
+
+
+		# self.BG_watermark_stensil = ((self.BG_watermark_stensil.astype("float32") * cv2.cvtColor(self.BG_watermark_stensil_alpha, cv2.COLOR_GRAY2BGR))*255).astype("uint8")
+
+
+		# self.BG_watermark_stensil_alpha_inv = ((1 - cv2.cvtColor(self.BG_watermark_stensil_alpha, cv2.COLOR_GRAY2BGR))*255).astype("uint8")
+		# self.BG_watermark_stensil_alpha_inv = cv2.cvtColor(self.BG_watermark_stensil_alpha_inv, cv2.COLOR_BGR2GRAY)
+		
+		# self.BG_watermark_stensil_alpha = (self.BG_watermark_stensil_alpha*255).astype("uint8")
+		# self.BG_watermark_stensil_alpha = cv2.cvtColor(self.BG_watermark_stensil_alpha, cv2.COLOR_BGR2GRAY)
+		
+		# t = cv2.resize(self.BG_watermark_stensil_alpha, (int(self.BG_watermark_stensil_alpha.shape[1]*0.7), int(self.BG_watermark_stensil_alpha_inv.shape[0]*0.7)))
+
+		# cv2.imshow("bg_sten", t)
+		# cv2.waitKey(0)
+
+	def MakeDiskWatermarkStensil(self):
+		self.disk_watermark_stensil = np.zeros((self.height, self.width, 3)).astype("uint8")
+		self.disk_watermark_stensil_alpha = np.zeros((self.height, self.width)).astype("uint8")
+
+		im = cv2.imread(self.logo_text_watermark_path, cv2.IMREAD_UNCHANGED)
+
+
+		x1 = self.width // 2 - im.shape[1] // 2
+		x2 = x1 + im.shape[1]
+
+		y1 = self.height // 2 - im.shape[0] // 2
+		y2 = y1 + im.shape[0]
+
+		self.disk_watermark_stensil[y1:y2, x1:x2] = im[:, :, :-1]
+		self.disk_watermark_stensil_alpha[y1:y2, x1:x2] = im[:, :, -1]
+
+		self.disk_watermark_stensil = cv2.bitwise_and(self.disk_watermark_stensil, self.disk_watermark_stensil, mask = self.disk_watermark_stensil_alpha)
+		self.disk_watermark_stensil_alpha_inv = cv2.bitwise_not(self.disk_watermark_stensil_alpha)
+
+
 
 	def ShowWatermarks(self):
 		self.frame[:, :, 2] = 255
@@ -45,14 +127,14 @@ class DiskRotation:
 
 		self.radians_per_second = self.rpm*2*np.pi / 60
 
-		self.disk_mask = np.zeros((self.height, self.width, 3)).astype("uint8")
-		cv2.circle(self.disk_mask, (self.width // 2, self.height // 2), self.disk_radius, (255, 255, 255), -1)
+		self.frame = np.zeros((self.height, self.width, 3))
+		self.disk_mask = np.zeros((self.height, self.width)).astype("uint8")
+		cv2.circle(self.disk_mask, (self.width // 2, self.height // 2), self.disk_radius, 255, -1)
+		self.disk_mask_inv = cv2.bitwise_not(self.disk_mask)
 
-		self.disk_mask = self.disk_mask.astype("float32") / 255
-
+		# self.disk_mask = self.disk_mask.astype("float32") / 255
 
 		self.Clear()
-
 
 	def FormatImage(self, image, new_width, clip_height_to):
 		H, W, _ = image.shape
@@ -100,6 +182,16 @@ class DiskRotation:
 
 	def DrawWatermark(self, image, image_type = "bg"):
 		if image_type == "bg":
+			# image = cv2.bitwise_and(image, image, mask = self.BG_watermark_stensil_alpha_inv)
+			# image = image + self.BG_watermark_stensil
+
+
+			# t = cv2.resize(self.BG_watermark_stensil, (int(self.BG_watermark_stensil_alpha.shape[1]*0.7), int(self.BG_watermark_stensil_alpha_inv.shape[0]*0.7)))
+
+			# cv2.imshow("t", t)
+			# cv2.waitKey(0)
+
+
 			im1 = self.logo_watermark_image.copy()
 			im2 = self.created_at_watermark_image.copy()
 
@@ -129,6 +221,9 @@ class DiskRotation:
 
 
 		elif image_type == "disk":
+			# image = cv2.bitwise_and(image, image, mask = self.disk_watermark_stensil_alpha_inv)
+			# image = image + self.disk_watermark_stensil
+
 			im = self.logo_text_watermark_image.copy()
 
 			im_alpha = cv2.cvtColor(im[:, :, -1], cv2.COLOR_GRAY2BGR).astype("float32") / 255
@@ -146,43 +241,46 @@ class DiskRotation:
 
 		return image
 
+	def PadImage(self, img, new_img_size):
+		w, h = new_img_size[0], new_img_size[1]
+		img_h, img_w, _ = img.shape
+
+		img_x1 = w // 2 - img_w // 2
+		img_y1 = h // 2 - img_h // 2
+
+		img_x2 = w // 2 + img_w // 2
+		img_y2 = h // 2 + img_h // 2
+
+
+		padded_img = np.zeros((h, w, 3)).astype("uint8")
+		padded_img[img_y1:img_y2, img_x1:img_x2] = img
+
+		return padded_img
 
 	def DrawDisk(self, disk_image, background_image, t):
 		angle = -t*self.radians_per_second
 		disk = self.RotateImage(disk_image.copy(), angle)
 
-		h, w, _ = self.frame.shape
-		disk_h, disk_w, _ = disk.shape
+		disk_frame = disk.copy()
 
-		disk_x1 = w // 2 - disk_w // 2
-		disk_y1 = h // 2 - disk_h // 2
+		disk_frame = cv2.bitwise_and(disk_frame, disk_frame, mask = self.disk_mask)
 
-		disk_x2 = w // 2 + disk_w // 2
-		disk_y2 = h // 2 + disk_h // 2
+		# background_image[self.disk_mask == 0] = disk_frame[self.disk_mask == 0]
 
-
-		disk_frame = np.zeros((self.height, self.width, 3)).astype("uint8")
-		disk_frame[disk_y1:disk_y2, disk_x1:disk_x2] = disk
-		disk_frame = disk_frame.astype("float32") / 255
-
-		background_frame = background_image.copy().astype("float32") / 255
-
-
-
-		self.frame = ((background_frame*(1 - self.disk_mask) + disk_frame*self.disk_mask)*255).astype("uint8")
+		return disk_frame + background_image
 
 	def RotateImage(self, image, angle):
-	    row, col = image.shape[:2]
-	    center = tuple(np.array([row,col])/2)
-	    rot_mat = cv2.getRotationMatrix2D(center, angle, 1.0)
-	    new_image = cv2.warpAffine(image, rot_mat, (col, row))
-	    return new_image
+		row, col = image.shape[:2]
+		center = tuple(np.array([row,col])/2)
+		rot_mat = cv2.getRotationMatrix2D(center, angle, 1.0)
+		new_image = cv2.warpAffine(image, rot_mat, (col, row))
+		return new_image
 
 	def Clear(self):
 		self.frame = np.zeros((self.height, self.width, 3))
+		# self.frame.fill(0)
 
-
-	def CreateVideoFrames(self, time, use_watermark = True, background_mode = 0, background_image_data = None, disk_image_data = None,  temp_video_filename = "video_temp.avi"):
+	def CreateVideoFrames(self, video_time, use_watermark = True, background_mode = 0, background_image_data = None, disk_image_data = None,  temp_video_filename = "video_temp.avi"):
 		self.use_watermark = use_watermark
 		total_frames = None
 		bg_video_cap = None
@@ -203,6 +301,8 @@ class DiskRotation:
 			if self.use_watermark:
 				bg_image = self.DrawWatermark(bg_image, "bg")
 
+			bg_image = cv2.bitwise_and(bg_image, bg_image, mask = self.disk_mask_inv)
+
 
 		elif background_mode == "image":
 			bg_image = cv2.imread(background_image_data)
@@ -210,6 +310,9 @@ class DiskRotation:
 
 			if self.use_watermark:
 				bg_image = self.DrawWatermark(bg_image, "bg")
+
+			bg_image = cv2.bitwise_and(bg_image, bg_image, mask = self.disk_mask_inv)
+
 
 
 		elif background_mode == "video":
@@ -219,13 +322,14 @@ class DiskRotation:
 
 		disk_image = cv2.imread(disk_image_data)
 		disk_image = self.FormatImage(disk_image, new_width = self.disk_radius*2, clip_height_to = self.disk_radius*2)
+		disk_image = self.PadImage(disk_image, (self.width, self.height))
 
 		if self.use_watermark:
 			disk_image = self.DrawWatermark(disk_image, "disk")
 
 
 
-		num_frames = int(time*self.fps)
+		num_frames = int(video_time*self.fps)
 		delta_t = 1 / self.fps
 
 
@@ -233,14 +337,13 @@ class DiskRotation:
 		writer = cv2.VideoWriter(temp_video_filename,  cv2.VideoWriter_fourcc(*'MJPG'), self.fps, size) 
 
 
-
+		tick = time.time()
 		bg_img_idx = 0
 		t = 0
 		for i in range(num_frames):
-			if i % 1000 == 0:
-				print(f"{i} / {num_frames} completed")
-
-			self.Clear()
+			# if i % 1000 == 0:
+			# 	print(f"{i} / {num_frames} completed")
+			# self.Clear()
 
 
 			if background_mode == "video":
@@ -253,27 +356,46 @@ class DiskRotation:
 				if self.use_watermark:
 					bg_image = self.DrawWatermark(bg_image, "bg")
 
-			self.DrawDisk(disk_image, bg_image, t)
-			writer.write(self.frame.astype("uint8"))
+				bg_image = cv2.bitwise_and(bg_image, bg_image, mask = self.disk_mask_inv)
 
-			# cv2.imshow("F", self.frame)
-			# k = cv2.waitKey(1)
 
-			# if k == ord("q"):
-			# 	break
+			self.frame = self.DrawDisk(disk_image, bg_image, t)
+			writer.write(self.frame)
+
 
 			t += delta_t
 
+		tock = time.time()
+
+		print(f"Time taken in generative video frames: {tock - tick}")
 
 		writer.release()
 		return True
 
 if __name__ == "__main__":
+	background_mode = "video"
+	audio_file_url = "D:/zain_dev/python_dev/rotating_disk/data/audio_1.5_min.mp3"
+	# audio_file_url = "D:/zain_dev/python_dev/rotating_disk/data/audio.mp3"
+	disk_image_data = "D:/zain_dev/python_dev/rotating_disk/data/disk.jpg"
+	temp_video_filename = "../temp.avi"
+
+
+	if background_mode == "image":
+		background_image_data = "D:/zain_dev/python_dev/rotating_disk/data/BG.jpeg"
+	if background_mode == "video":	
+		background_image_data = "D:/zain_dev/python_dev/rotating_disk/data/BG_Vid.mp4"
+	else:
+		background_image_data = '#ff0000'
+		background_image_data = (0, 255, 0)
 	DR = DiskRotation(1080, 1080, disk_radius = int((1080/2)*0.8), rpm = 200, fps = 30)
-	DR.Clear()
-	DR.ShowWatermarks()
 
-	# cv2.imshow("frame", DR.frame)
-	# cv2.waitKey(0)
+	sound_data, fs = sf.read(audio_file_url, dtype='float32')
 
-	cv2.imwrite("test.png", DR.frame)
+	video_time = sound_data.shape[0] / fs
+
+	t1 = time.time()
+	DR.CreateVideoFrames(video_time, use_watermark = True, background_mode = background_mode, background_image_data = background_image_data, 
+						disk_image_data = disk_image_data, temp_video_filename = temp_video_filename)
+	t2 = time.time()
+
+	print(f"Total Time taken: {t2 -t1} s")
